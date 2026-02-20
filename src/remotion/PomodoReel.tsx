67034e45b095dc9c947dbb4bc77fab372c3b31c6
Charles Dyas <dyascj@gmail.com>
2026-02-20 12:49:53 -0500

    feat: add TimerScene and TasksScene

--- src/remotion/PomodoReel.tsx
+++ src/remotion/PomodoReel.tsx
@@ -1,11 +1,19 @@
 import { AbsoluteFill, Sequence } from "remotion";
 import { SplashScene } from "./scenes/SplashScene";
+import { TimerScene } from "./scenes/TimerScene";
+import { TasksScene } from "./scenes/TasksScene";
 
 export const PomodoReel: React.FC = () => {
   return (
     <AbsoluteFill>
       <Sequence from={0} durationInFrames={90} name="Splash">
         <SplashScene />
+      </Sequence>
+      <Sequence from={90} durationInFrames={90} name="Timer Appears">
+        <TimerScene />
+      </Sequence>
+      <Sequence from={180} durationInFrames={90} name="Tasks Fly In">
+        <TasksScene />
       </Sequence>
     </AbsoluteFill>
   );
